8cb3bf24d5dbc6f2bcfcfdef55682cf2a2293595
Davis <dbkeene.tsyc@gmail.com>
2017-08-06 15:32:27 -0400

    Added skill to zappa, experiencing error with nltk.download('punkt'). Skill works on ngrok, working on getting skill up and running on zappa and aws.

--- theraipest.py
+++ theraipest.py
@@ -1,15 +1,30 @@
 from flask import Flask
 from flask_ask import Ask, question, statement
-import theraipy
+import pandas as pd
+import numpy as np
 import nltk
 from nltk.tokenize import word_tokenize
 
 app = Flask(__name__)
 ask = Ask(app, "/")
 
+#Import the data from a csv file
+
+csvdata = pd.read_csv('training_data.csv', skipinitialspace=True,delimiter=",")
+
+#Convert data into a numpy array
+csvdata1 = np.array(csvdata)
+train = []
+
+#Put contents of numpy array into empty list
+train.extend(csvdata1)
+
+nltk.download('punkt')
+
+#Process the user input by tokenizing it
 @ask.launch
 def startSkill():
-    msg = "Hello, welcome to the Ther-AI-pest. Can I help you?"
+    msg = "Hello, welcome to the There-A.I.-pest. Can I help you?"
     return question(msg)
 
 @ask.intent("YesIntent")
@@ -19,35 +34,50 @@ def theraipy():
 
 @ask.intent("NoIntent")
 def happy():
-    msg = "I'm glad that you are feeling happy. What was the best part of your day?"
+    msg = "Okay. If you need anything, I'll be waiting!"
+    return statement(msg)
+
+@ask.intent("AdviceIntent")
+def give_advice():
+    msg = "What do you need help with?"
     return question(msg)
-
-@ask.intent("ElaborateIntent")
-def elab(extension):
-    try:
-        process = theraipy.elaborate(extension)
-        return process
-
-    except:
-        msg = "Sorry, I must have misunderstood you. Can you repeat that?"
-        return question(msg)
 
 @ask.intent("TherapyIntent")
 def theraipy(line):
     try:
-        sentiment = theraipy.process(line)
 
-        if sentiment == "pos":
-            msg = "I am glad that you are feeling well. Would you like to continue to speak?"
+        wordlist = set(word.lower() for statement in train for word in word_tokenize(statement[0]))
+
+        x = [({word: (word in word_tokenize(x[0])) for word in wordlist}, x[1]) for x in train]
+
+        classifier = nltk.NaiveBayesClassifier.train(x)
+
+        test_data = line
+        if line.__len__() > 1:
+
+            # If there is a string, return if string is labeled as positive or negative
+            test_data_features = {word.lower(): (word in word_tokenize(test_data.lower())) for word in wordlist}
+            sentiment = (classifier.classify(test_data_features))
+        else:
+            msg = "I'm sorry, can you repeat that?"
             return question(msg)
+        if sentiment == "pos":
+            line.replace("am", "are")
+            line.replace("I","you")
+            line.replace("I'm","you're")
+            msg = "I am glad that "+line+" . Would you like to tell me more?"
+            return question(msg)
+
         if sentiment == "neg":
             if "kill myself" in line:
-                msg = "I am sorry that you are feeling suicidal."
-                return statement(msg)
+                msg = "It appears that you are feeling suicidal. Please, tell me more. Why are you feeling this way?"
+                return question(msg)
+            else:
+                msg = "I'm sorry that you're feeling sad. Can you elaborate?"
+                return question(msg)
 
-    except:
-        msg = "Sorry, I must have misunderstood you. Can you try again?"
-        return question(msg)
+
+    except Exception, e: return question(str(e))
 
 if __name__ == '__main__':
     app.run(debug=True)
